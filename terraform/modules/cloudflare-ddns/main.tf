3a42cf6d0eb77aabc3de82de1ea44e160d025527
valiantlynx <gormerykombo@gmail.com>
2024-03-23 22:46:30 +0100

    passed validate

--- terraform/modules/cloudflare-ddns/main.tf
+++ terraform/modules/cloudflare-ddns/main.tf
@@ -1,35 +1,34 @@
 locals {
-  dns_records = tomap(flatten([
-    for domain, details in var.cloudflare_zone_ids : [
-      // A records for root domain, if included
-      if details.include_root {
-        "${domain}" = {
-          zone_id = details.zone_id,
-          name    = domain,
-          type    = "A",
-          value   = var.public_ip
-        }
-      },
-      // A or CNAME records for subdomains
-      if details.include_subdomains {
-        for subdomain in details.subdomains : "${subdomain.name}.${domain}" = {
-          zone_id = details.zone_id,
-          name    = "${subdomain.name}.${domain}",
-          type    = subdomain.name == "www" ? "CNAME" : "A", // Use CNAME for 'www', A for others
-          value   = subdomain.name == "www" ? domain : var.public_ip // Point 'www' to root domain, others to IP
-        }
-      }
-    ]
-  ]))
+  root_dns_records = {
+    for domain, details in var.cloudflare_zone_ids : domain => {
+      zone_id = details.zone_id
+      name    = domain
+      type    = "A"
+      value   = var.public_ip
+    } if details.include_root
+  }
+
+  subdomain_dns_records = merge([
+    for domain, details in var.cloudflare_zone_ids : {
+      for subdomain in details.subdomains : "${subdomain.name}.${domain}" => {
+        zone_id = details.zone_id
+        name    = "${subdomain.name}.${domain}"
+        type    = subdomain.name == "www" ? "CNAME" : "A"
+        value   = subdomain.name == "www" ? domain : var.public_ip
+      }...
+    } if details.include_subdomains
+  ]...)
+
+  combined_dns_records = merge(local.root_dns_records, local.subdomain_dns_records)
 }
 
 resource "cloudflare_record" "ec2_dns_records" {
-  for_each = locals.dns_records
+  for_each = local.combined_dns_records
 
-  zone_id  = each.value.zone_id
-  name     = each.value.name
-  value    = each.value.value
-  type     = each.value.type
-  ttl      = 1 // Auto, use a specific TTL if needed
-  proxied  = false
+  zone_id = each.value.zone_id
+  name    = each.value.name
+  value   = each.value.value
+  type    = each.value.type
+  ttl     = 1 // Auto, use a specific TTL if needed
+  proxied = false
 }
